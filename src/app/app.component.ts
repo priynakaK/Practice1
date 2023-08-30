1752627b5cf28959fb3cfce2507695eb494ca8c1
priynakaK <140493065+priynakaK@users.noreply.github.com>
2023-08-30 17:23:38 +0530

    2 files modified, 6 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { MiddleComponent } from './middle/middle.component';
 
 @Component({
   standalone: true,
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [FooterComponent, MiddleComponent, FooterComponent],
 })
 export class AppComponent implements OnInit {
   constructor() {}
---
+++ src/app/middle/middle.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-middle',
+  templateUrl: './middle.component.html',
+  styleUrls: ['./middle.component.css'],
+})
+export class MiddleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
